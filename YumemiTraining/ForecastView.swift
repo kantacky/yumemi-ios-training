c6eed150e8e387f3b63823fc7f9fe95970defdc5
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-16 14:03:17 +0900

    Centerize image and labeles, except buttons

--- YumemiTraining/ForecastView.swift
+++ YumemiTraining/ForecastView.swift
@@ -8,52 +8,64 @@
 import SwiftUI
 
 struct ForecastView: View {
+    @State private var labelHeight: CGFloat = 0
+
     var body: some View {
         GeometryReader { geometry in
-            VStack(spacing: 80) {
-                VStack {
-                    ZStack {
-                        Rectangle()
-                            .fill(.gray)
-                        
-                        Text("Image")
-                            .foregroundStyle(.white)
-                    }
+            VStack {
+                // Image
+                Image("")
                     .frame(
                         width: geometry.size.width / 2,
                         height: geometry.size.width / 2
                     )
-                    
-                    HStack(spacing: 0) {
-                        Text("--")
-                            .foregroundStyle(.blue)
-                            .frame(width: geometry.size.width / 4)
-                        
-                        Text("--")
-                            .foregroundStyle(.red)
-                            .frame(width: geometry.size.width / 4)
-                    }
-                }
+                    .border(Color.black)
                 
+                // Labels
                 HStack(spacing: 0) {
-                    Button {
-                        // TODO: Close Action
-                    } label: {
-                        Text("Close")
+                    Text("--")
+                        .foregroundStyle(.blue)
+                        .frame(width: geometry.size.width / 4)
+                    
+                    Text("--")
+                        .foregroundStyle(.red)
+                        .frame(width: geometry.size.width / 4)
+                }
+                .background {
+                    GeometryReader { labelGeometry in
+                        Path { _ in
+                            Task {
+                                self.labelHeight = labelGeometry.size.height
+                                print(self.labelHeight)
+                            }
+                        }
                     }
-                    .frame(width: geometry.size.width / 4)
-
-                    Button {
-                        // TODO: Reload Action
-                    } label: {
-                        Text("Reload")
-                    }
-                    .frame(width: geometry.size.width / 4)
                 }
             }
             .position(
                 x: geometry.frame(in: .local).midX,
                 y: geometry.frame(in: .local).midY
+            )
+
+            // Buttons
+            HStack(spacing: 0) {
+                Button {
+                    // TODO: Close Action
+                } label: {
+                    Text("Close")
+                }
+                .frame(width: geometry.size.width / 4)
+                
+                Button {
+                    // TODO: Reload Action
+                } label: {
+                    Text("Reload")
+                }
+                .frame(width: geometry.size.width / 4)
+            }
+            .position(
+                x: geometry.frame(in: .local).midX,
+                y: geometry.frame(in: .local).midY + geometry.size.width / 4 + labelHeight + 8 + 80
             )
         }
     }
