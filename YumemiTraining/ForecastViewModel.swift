b96fa5bcab3387fd0ef2a924de59c9b60a4f00fa
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-16 14:48:49 +0900

    Add weather images

---
+++ YumemiTraining/ForecastViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ForecastViewModel.swift
+//  YumemiTraining
+//
+//  Created by 及川 寛太 on 2024/02/16.
+//
+
+import Foundation
---
+++ YumemiTraining/WeatherCondition.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherCondition.swift
+//  YumemiTraining
+//
+//  Created by 及川 寛太 on 2024/02/16.
+//
+
+import Foundation
