24023b6460b6d2f299484249f2cc20da641c0667
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-16 11:23:17 +0900

    Initial commit

---
+++ iOSTrainingOikawa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTrainingOikawa
+//
+//  Created by 及川 寛太 on 2024/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSTrainingOikawaUITests/iOSTrainingOikawaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOSTrainingOikawaUITestsLaunchTests.swift
+//  iOSTrainingOikawaUITests
+//
+//  Created by 及川 寛太 on 2024/02/16.
+//
+
+import XCTest
+
+final class iOSTrainingOikawaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
