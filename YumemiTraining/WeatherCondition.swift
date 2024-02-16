ef41202f8430a115f3d39d6b7e61df353996194e
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-16 14:49:21 +0900

    Add weather condition enum

--- YumemiTraining/WeatherCondition.swift
+++ YumemiTraining/WeatherCondition.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum WeatherCondition {
+    case sunny
+    case cloudy
+    case rainy
+    
+    var image: Image {
+        switch self {
+        case .sunny:
+            return .init(.sunny)
+            
+        case .cloudy:
+            return .init(.cloudy)
+            
+        case .rainy:
+            return .init(.rainy)
+        }
+    }
+    
+    static func from(string: String) -> Self? {
+        switch string {
+        case "sunny":
+            return .sunny
+            
+        case "cloudy":
+            return .cloudy
+            
+        case "rainy":
+            return .rainy
+            
+        default:
+            return nil
+        }
+    }
+}
