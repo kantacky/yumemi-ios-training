624e10811101db2cf501dee430f554eeb72f2763
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-16 15:50:25 +0900

    Add error handling while calling API method

--- YumemiTraining/ForecastViewModel.swift
+++ YumemiTraining/ForecastViewModel.swift
@@ -12,18 +12,52 @@ import YumemiWeather
 protocol ForecastViewModel: ObservableObject {
     
     var weatherCondition: WeatherCondition? { get }
+    var alert: String? { get }
 
     func reload() -> Void
+    func dismissAlert() -> Void
 }
 
 @MainActor
 final class ForecastViewModelImpl: ForecastViewModel {
     
     @Published private (set) var weatherCondition: WeatherCondition?
+    @Published private (set) var alert: String?
 
     func reload() {
+        self.weatherCondition = nil
+        self.fetchThrowingWeatherCondition(at: "tokyo")
+    }
+    
+    func dismissAlert() {
+        self.alert = nil
+        self.reload()
+    }
+    
+    private func fetchWeatherCondition() {
         let weatherConditionString: String = YumemiWeather.fetchWeatherCondition()
 
         self.weatherCondition = .init(rawValue: weatherConditionString)
+    }
+    
+    private func fetchThrowingWeatherCondition(at: String) {
+        do {
+            let weatherConditionString: String = try YumemiWeather.fetchWeatherCondition(at: at)
+            
+            self.weatherCondition = .from(string: weatherConditionString)
+        } catch {
+            debugPrint(error.localizedDescription)
+
+            switch error {
+            case YumemiWeatherError.invalidParameterError:
+                self.alert = "Area is invalid."
+                
+            case YumemiWeatherError.unknownError:
+                self.alert = "Unknown error has occurred while fetching weather condition."
+                
+            default:
+                self.alert = "Unknown error has occurred while fetching weather condition."
+            }
+        }
     }
 }
