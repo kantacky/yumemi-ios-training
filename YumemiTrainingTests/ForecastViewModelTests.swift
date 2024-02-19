b9aa71cfc0abdeedc889e33e586fec2aa565b81b
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-19 12:28:29 +0900

    Add ForecastViewModelTests

--- YumemiTrainingTests/ForecastViewModelTests.swift
+++ YumemiTrainingTests/ForecastViewModelTests.swift
@@ -5,4 +5,64 @@
 //  Created by 及川 寛太 on 2024/02/19.
 //
 
-import Foundation
+import XCTest
+@testable import YumemiTraining
+
+@MainActor
+final class ForecastViewModelTests: XCTestCase {
+
+    var viewModel: any ForecastViewModel = ForecastViewModelMock()
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+
+    func testReload() {
+        let expectedWeatherCloudy: Weather = .init(
+            date: .now,
+            weatherCondition: .cloudy,
+            maxTemperature: 10,
+            minTemperature: -10
+        )
+
+        let expectedWeatherSunny: Weather = .init(
+            date: .now,
+            weatherCondition: .sunny,
+            maxTemperature: 10,
+            minTemperature: -10
+        )
+
+        let expectedWeatherRainy: Weather = .init(
+            date: .now,
+            weatherCondition: .rainy,
+            maxTemperature: 10,
+            minTemperature: -10
+        )
+
+        self.viewModel.reload()
+        XCTAssertEqual(self.viewModel.errorMessage, "There was an error fetching the weather.")
+        XCTAssertEqual(self.viewModel.weather?.weatherCondition, expectedWeatherSunny.weatherCondition)
+        XCTAssertEqual(self.viewModel.weather?.maxTemperature, expectedWeatherSunny.maxTemperature)
+        XCTAssertEqual(self.viewModel.weather?.minTemperature, expectedWeatherSunny.minTemperature)
+
+        self.viewModel.dismissAlert()
+        XCTAssertEqual(self.viewModel.errorMessage, nil)
+        XCTAssertEqual(self.viewModel.weather?.weatherCondition, expectedWeatherCloudy.weatherCondition)
+        XCTAssertEqual(self.viewModel.weather?.maxTemperature, expectedWeatherCloudy.maxTemperature)
+        XCTAssertEqual(self.viewModel.weather?.minTemperature, expectedWeatherCloudy.minTemperature)
+
+        self.viewModel.reload()
+        XCTAssertEqual(self.viewModel.weather?.weatherCondition, expectedWeatherRainy.weatherCondition)
+        XCTAssertEqual(self.viewModel.weather?.maxTemperature, expectedWeatherRainy.maxTemperature)
+        XCTAssertEqual(self.viewModel.weather?.minTemperature, expectedWeatherRainy.minTemperature)
+
+        self.viewModel.reload()
+        XCTAssertEqual(self.viewModel.weather?.weatherCondition, expectedWeatherSunny.weatherCondition)
+        XCTAssertEqual(self.viewModel.weather?.maxTemperature, expectedWeatherSunny.maxTemperature)
+        XCTAssertEqual(self.viewModel.weather?.minTemperature, expectedWeatherSunny.minTemperature)
+    }
+}
--- YumemiTrainingTests/YumemiTrainingTests.swift
+++ YumemiTrainingTests/YumemiTrainingTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 
+@MainActor
 final class YumemiTrainingTests: XCTestCase {
 
     override func setUpWithError() throws {
