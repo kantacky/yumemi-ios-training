a3475a3a60909de59e2547c8393805b910e0b6f6
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-19 15:31:43 +0900

    Update ForecastViewUITests

--- YumemiTrainingUITests/ForecastViewUITests.swift
+++ YumemiTrainingUITests/ForecastViewUITests.swift
@@ -23,7 +23,7 @@ final class ForecastViewUITests: XCTestCase {
         try super.tearDownWithError()
     }
 
-    func testOnAppear() throws {
+    func testOnAppearSunny() throws {
         let imageName = try view.inspect().geometryReader().vStack().group(0).image(0).actualImage().name()
         let minTemperature = try view.inspect().geometryReader().vStack().hStack(0).text(0).string()
         let maxTemperature = try view.inspect().geometryReader().vStack().hStack(0).text(1).string()
@@ -32,12 +32,22 @@ final class ForecastViewUITests: XCTestCase {
         XCTAssertEqual(maxTemperature, "10")
     }
 
-    func testReload() throws {
+    func testReloadCloudy() throws {
         try view.inspect().geometryReader().hStack().button(1).tap()
         let imageName = try view.inspect().geometryReader().vStack().group(0).image(0).actualImage().name()
         let minTemperature = try view.inspect().geometryReader().vStack().hStack(0).text(0).string()
         let maxTemperature = try view.inspect().geometryReader().vStack().hStack(0).text(1).string()
         XCTAssertEqual(imageName, "Cloudy")
+        XCTAssertEqual(minTemperature, "-10")
+        XCTAssertEqual(maxTemperature, "10")
+    }
+
+    func testReloadRainy() throws {
+        try view.inspect().geometryReader().hStack().button(1).tap()
+        let imageName = try view.inspect().geometryReader().vStack().group(0).image(0).actualImage().name()
+        let minTemperature = try view.inspect().geometryReader().vStack().hStack(0).text(0).string()
+        let maxTemperature = try view.inspect().geometryReader().vStack().hStack(0).text(1).string()
+        XCTAssertEqual(imageName, "Rainy")
         XCTAssertEqual(minTemperature, "-10")
         XCTAssertEqual(maxTemperature, "10")
     }
