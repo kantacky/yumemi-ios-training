03f7bb127efa09f248552e8a3150ec58da3c2b56
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-20 18:36:44 +0900

    Add WeatherListView

--- YumemiTraining/WeatherListView.swift
+++ YumemiTraining/WeatherListView.swift
@@ -6,13 +6,78 @@
 //
 
 import SwiftUI
+import YumemiWeather
 
 struct WeatherListView: View {
+    @Environment(\.scenePhase) private var scenePhase
+    @ObservedObject var viewModel: WeatherListViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(viewModel.weatherList, id: \.self) { weather in
+                NavigationLink(value: weather) {
+                    HStack {
+                        HStack {
+                            weather.info.weatherCondition.image
+                                .scaledToFit()
+                                .aspectRatio(1, contentMode: .fit)
+                                .frame(height: 24)
+
+                            Text(weather.area)
+                        }
+
+                        Spacer()
+
+                        HStack {
+                            Text("\(weather.info.minTemperature)")
+                                .foregroundStyle(.blue)
+                            Text("/")
+                                .foregroundStyle(.secondary)
+                            Text("\(weather.info.maxTemperature)")
+                                .foregroundStyle(.red)
+                        }
+                        .fontDesign(.monospaced)
+                    }
+                }
+            }
+            .navigationDestination(for: Weather.self) { weather in
+                ForecastView(viewModel: ForecastViewModel(weather: weather))
+            }
+            .refreshable {
+                await viewModel.reload(areas: Area.allCases.map { $0.rawValue }, date: .now)
+            }
+        }
+        .alert(
+            .init("Error"),
+            isPresented: .constant(viewModel.errorMessage != nil)
+        ) {
+            Button {
+                viewModel.dismissAlert()
+            } label: {
+                Text("OK")
+            }
+        } message: {
+            if let text = viewModel.errorMessage {
+                Text(text)
+            }
+        }
+        .task {
+            await viewModel.reload(areas: Area.allCases.map { $0.rawValue }, date: .now)
+        }
+        .onChange(of: scenePhase) { _, newValue in
+            switch newValue {
+            case .active:
+                Task {
+                    await viewModel.reload(areas: Area.allCases.map { $0.rawValue }, date: .now)
+                }
+
+            default:
+                return
+            }
+        }
     }
 }
 
 #Preview {
-    WeatherListView()
+    WeatherListView(viewModel: WeatherListViewModel())
 }
