f2a70c3e75e56b66f4070ef253b927b3a2e0444c
Kanta Oikawa <k_oikawa@yumemi.co.jp>
2024-02-27 14:31:45 +0900

    Refactor

--- YumemiTraining/YumemiWeatherClient.swift
+++ YumemiTraining/YumemiWeatherClient.swift
@@ -28,14 +28,16 @@ extension YumemiWeatherClient: DependencyKey {
             @Dependency(Encoder.self) var weatherEncoder
             @Dependency(Decoder.self) var weatherDecoder
             let request = WeatherRequest(area: area, date: date)
-            let response = try await YumemiWeather.asyncFetchWeather(try weatherEncoder.encodeWeatherInfoRequest(request))
+            let encodedRequest = try weatherEncoder.encodeWeatherInfoRequest(request)
+            let response = try await YumemiWeather.asyncFetchWeather(encodedRequest)
             return try weatherDecoder.decodeWeatherInfoResponse(response)
         },
         fetchWeatherList: { areas, date in
             @Dependency(Encoder.self) var weatherEncoder
             @Dependency(Decoder.self) var weatherDecoder
             let request = WeatherListRequest(areas: areas, date: date)
-            let response = try await YumemiWeather.asyncFetchWeatherList(try weatherEncoder.encodeWeatherListRequest(request))
+            let encodedRequest = try weatherEncoder.encodeWeatherListRequest(request)
+            let response = try await YumemiWeather.asyncFetchWeatherList(encodedRequest)
             return try weatherDecoder.decodeWeatherListResponse(response)
         }
     )
